b93f4e6ee7c34f2fa7f9efeabbafd7b4560baaf9
Your Name <ffilip.djokic@gmail.com>
2022-03-16 20:43:20 +0100

    commit

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -58,10 +58,10 @@ const Wrapper = styled.div`
   @media only screen and (max-width: 992px) {
     .sidebar {
       position: fixed;
-      top: 9.6%;
+      top: 0;
       left: 0;
-      width: 50%;
-      height: 50%;
+      width: 100%;
+      height: 100%;
       height: 100vh;
       z-index: -1;
       background-image: linear-gradient(147deg, #000000 0%, #434343 74%);
