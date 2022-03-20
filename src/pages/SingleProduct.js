992099d9b1ac6cd1795c74af82a9e77c1a579b1c
Your Name <ffilip.djokic@gmail.com>
2022-03-20 17:54:38 +0100

    commit

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -106,8 +106,6 @@ const Wrapper = styled.div`
   @media only screen and (max-width: 992px) {
   }
   @media only screen and (max-width: 768px) {
-  }
-  @media only screen and (max-width: 520px) {
     img {
       width: 90%;
     }
